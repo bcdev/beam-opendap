6a60f7b70a30f6d8c727bac4ed519659cb5e2e55
thomasstorm <thomas_storm@brockmann-consult.de>
2012-07-19 11:55:00 +0200

    removed warning

--- src/main/java/org/esa/beam/opendap/utils/OpendapUtils.java
+++ src/main/java/org/esa/beam/opendap/utils/OpendapUtils.java
@@ -21,7 +21,13 @@ import opendap.dap.http.HTTPSession;
 import org.esa.beam.opendap.OpendapLeaf;
 import thredds.catalog.InvDatasetImpl;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 /**
  * Contains some methods useful when dealing with OPeNDAP servers.
@@ -39,6 +45,26 @@ public class OpendapUtils {
         HTTPSession session = new HTTPSession();
         final HTTPMethod httpMethod = session.newMethodGet(fileUri);
         httpMethod.execute();
-        return httpMethod.getResponseAsString();
+        InputStream responseStream = httpMethod.getResponseAsStream();
+        return convertStreamToString(responseStream);
+    }
+
+    private static String convertStreamToString(InputStream is) throws IOException {
+        if (is != null) {
+            Writer writer = new StringWriter();
+            char[] buffer = new char[1024];
+            try {
+                Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } finally {
+                is.close();
+            }
+            return writer.toString();
+        } else {
+            return "";
+        }
     }
 }
